a3e87f17344d375a39d9350d7f43d87e6a015946
Phillip Denness <dennessphillip@gmail.com>
2018-05-28 12:31:32 +0100

    Create negative match table

---
+++ src/main/scripts/create_negative_match_data.sql
@@ -0,0 +1,4 @@
+insert into ldf.negative_match values (1, "lenovo");
+insert into ldf.negative_match values (2, "hp");
+insert into ldf.negative_match values (3, "packard bell");
+insert into ldf.negative_match values (4, "samsung");
