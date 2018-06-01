d7aa6b030d8cc79ab50da26a30021bc9e447d377
Phillip Denness <dennessphillip@gmail.com>
2018-06-01 23:21:10 +0100

    Serve html

--- src/main/java/com/localdealfinder/Main.java
+++
@@ -1,6 +0,0 @@
-package com.localdealfinder;
-
-public class Main {
-
-
-}
--- src/main/java/com/localdealfinder/controller/HelloController.java
+++ src/main/java/com/localdealfinder/controller/HelloController.java
@@ -1,14 +1,24 @@
 package com.localdealfinder.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
     }
 
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
 }
