5d0dc1b7d3c7d11b8a762ffaee60e7d0ac94bc74
Phillip Denness <dennessphillip@gmail.com>
2018-06-04 21:36:06 +0100

    Thymeleaf html pages
    Added bootstrap and jquery to dependencies.
    Updated SQL test data file.

--- src/main/java/com/localdealfinder/controller/HelloController.java
+++ src/main/java/com/localdealfinder/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.localdealfinder.controller;
 
 
+import com.localdealfinder.Service.AdvertService;
+import com.localdealfinder.model.Advert;
+import com.localdealfinder.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HelloController {
@@ -18,7 +24,14 @@ public class HelloController {
     }
 
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+    public String greetingSubmit(Model model) {
+        List<Advert> adverts = null;
+        AdvertService service = AdvertService.getInstance();
+        Optional<List<Advert>> advertsOpt = service.readAll();
+        if(advertsOpt.isPresent()){
+            adverts = advertsOpt.get();
+        }
+        model.addAttribute("adverts", adverts);
         return "result";
     }
 }
--- src/main/scripts/create_search_data.sql
+++ src/main/scripts/create_search_data.sql
@@ -1,1 +1,2 @@
 insert into ldf.search values(1, "test_search", "london", 10, 100);
+insert into ldf.search values(2, "test_search2", "Sheffield", 100, 130);
